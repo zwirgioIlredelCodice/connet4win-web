bce2a9ad5bc1484c577095c6bd72d78c0b488054
Fabio Murer <murer.fabio23@gmail.com>
2023-09-27 10:31:52 +0200

    lil

--- assets/dioxus/snippets/dioxus-interpreter-js-603636eeca72cf05/inline0.js
+++ assets/dioxus/snippets/dioxus-interpreter-js-603636eeca72cf05/inline0.js
@@ -163,7 +163,7 @@ let m,p,ls,d,t,op,i,e,z,metaflags;
       function truthy(val) {
         return val === "true" || val === true;
       }
-    let u8buf,u8bufp;const attr = [];
+    let u32buf,u32bufp;const attr = [];
                     let attr_tmp1, attr_tmp2;
                     function get_attr() {
                         attr_tmp2 = u8buf[u8bufp++];
@@ -175,7 +175,7 @@ let m,p,ls,d,t,op,i,e,z,metaflags;
                         else{
                             return attr[attr_tmp2&4294967167];
                         }
-                    }const evt = [];
+                    }let u8buf,u8bufp;const evt = [];
                     let evt_tmp1, evt_tmp2;
                     function get_evt() {
                         evt_tmp2 = u8buf[u8bufp++];
@@ -187,7 +187,7 @@ let m,p,ls,d,t,op,i,e,z,metaflags;
                         else{
                             return evt[evt_tmp2&4294967167];
                         }
-                    }let u32buf,u32bufp;const ns_cache = [];
+                    }const ns_cache = [];
                     let ns_cache_tmp1, ns_cache_tmp2;
                     function get_ns_cache() {
                         ns_cache_tmp2 = u8buf[u8bufp++];
@@ -200,7 +200,7 @@ let m,p,ls,d,t,op,i,e,z,metaflags;
                             return ns_cache[ns_cache_tmp2&4294967167];
                         }
                     }let s = "";let lsp,sp,sl; let c = new TextDecoder();
-            let id,event_name,field,ptr,value,ns,len,bubbles;
+            let bubbles,field,id,len,ns,event_name,ptr,value;
             export function create(r){
                 d=r;
             }
@@ -213,13 +213,13 @@ let m,p,ls,d,t,op,i,e,z,metaflags;
                     ls=m.getUint32(d+12*4,true);
                 }
                 p=ls;
-                if ((metaflags>>>5)&1){
-                u8buf=new Uint8Array(m.buffer,m.getUint32(d+5*4,true))
-            }
-            u8bufp=0;if ((metaflags>>>3)&1){
+                if ((metaflags>>>3)&1){
                 u32buf=new Uint32Array(m.buffer,m.getUint32(d+3*4,true))
             }
-            u32bufp=0;if (metaflags&1){
+            u32bufp=0;if ((metaflags>>>5)&1){
+                u8buf=new Uint8Array(m.buffer,m.getUint32(d+5*4,true))
+            }
+            u8bufp=0;if (metaflags&1){
                 lsp = m.getUint32(d+1*4,true);
             }
             if ((metaflags>>>2)&1) {
